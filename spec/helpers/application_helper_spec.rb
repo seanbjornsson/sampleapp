e8cc49a1d499de99d373cf1845d4e756c04b587a
Sean Bjornsson <sean.bjornsson@annkissam.com>
2014-06-12 14:29:01 -0400

    resetting

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'ApplicationHelper' do 
+	include ApplicationHelper
+
+	context "full_title" do
+		context "without page_title" do
+			let(:page_title) {nil}
+
+			it "should return the base_title" do
+				expect(full_title(page_title). to eq('Ruby on Rails Tutorial Sample App'))
+			end
+		end
+
+		context "with a page_title" do
+			let (:page_title)  { 'Help' }
+
+			it "should return the base_title and page_title" do
+				
+				expect(full_title(page_title)).to eq("Ruby on Rails Tutorial Sample App | #{page_title}")
+			end
+		end
+	end
+end
